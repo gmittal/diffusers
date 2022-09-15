fcc494ec25cb2631f644ecd08e59cfeb2a6ec949
da03 <dengyuntian@gmail.com>
2022-09-15 16:37:41 -0400

    .

--- src/diffusers/pipelines/ddpm/pipeline_ddpm.py
+++ src/diffusers/pipelines/ddpm/pipeline_ddpm.py
@@ -111,3 +111,91 @@ class DDPMPipeline(DiffusionPipeline):
         #    image = self.numpy_to_pil(image)
 
         #return {"sample": image}
+
+    @torch.no_grad()
+    def run_clean(self, batch_size=1, generator=None, encoder_hidden_states=None, attention_mask=None, output_type="pil", **kwargs):
+        if "torch_device" in kwargs:
+            device = kwargs.pop("torch_device")
+            warnings.warn(
+                "`torch_device` is deprecated as an input argument to `__call__` and will be removed in v0.3.0."
+                " Consider using `pipe.to(torch_device)` instead."
+            )
+
+            # Set device as before (to be removed in 0.3.0)
+            if device is None:
+                device = "cuda" if torch.cuda.is_available() else "cpu"
+            self.to(device)
+
+        # Sample gaussian noise to begin loop
+        #import pdb; pdb.set_trace()
+        image = torch.randn(
+            (batch_size, self.unet.in_channels, self.unet.sample_size[0], self.unet.sample_size[1]),
+            generator=generator,
+        )
+        image = image.to(self.device)
+
+        # set step values
+        self.scheduler.set_timesteps(1000)
+
+        for t in self.progress_bar(self.scheduler.timesteps):
+            # 1. predict noise model_output
+            model_output = self.unet(image, t, encoder_hidden_states, attention_mask)["sample"]
+
+            # 2. compute previous image: x_t -> t_t-1
+            result = self.scheduler.step_clean(model_output, t, image, generator=generator)#["prev_sample"]
+            image_clean = result['orig_sample']
+            image = result['prev_sample']
+
+
+            image_yield = (image / 2 + 0.5).clamp(0, 1)
+            image_yield = image_yield.cpu().permute(0, 2, 3, 1).numpy()
+
+            image_clean_yield = (image_clean / 2 + 0.5).clamp(0, 1)
+            image_clean_yield = image_clean_yield.cpu().permute(0, 2, 3, 1).numpy()
+            yield image_yield, image_clean_yield
+
+    @torch.no_grad()
+    def swap(self, batch_size=1, generator=None, encoder_hidden_states=None, attention_mask=None, output_type="pil", swap_step=-1, **kwargs):
+        if "torch_device" in kwargs:
+            device = kwargs.pop("torch_device")
+            warnings.warn(
+                "`torch_device` is deprecated as an input argument to `__call__` and will be removed in v0.3.0."
+                " Consider using `pipe.to(torch_device)` instead."
+            )
+
+            # Set device as before (to be removed in 0.3.0)
+            if device is None:
+                device = "cuda" if torch.cuda.is_available() else "cpu"
+            self.to(device)
+
+        # Sample gaussian noise to begin loop
+        #import pdb; pdb.set_trace()
+        assert batch_size % 2 == 0, batch_size
+        half_batch_size = batch_size // 2
+        image = torch.randn(
+            (batch_size, self.unet.in_channels, self.unet.sample_size[0], self.unet.sample_size[1]),
+            generator=generator,
+        )
+        image = image.to(self.device)
+
+        # set step values
+        self.scheduler.set_timesteps(1000)
+        import pdb; pdb.set_trace()
+
+        for t in self.progress_bar(self.scheduler.timesteps):
+            if swap_step == t:
+                print ('-'*10)
+                print ('swap')
+                image[half_batch_size:] = image[:half_batch_size]
+            # 1. predict noise model_output
+            model_output = self.unet(image, t, encoder_hidden_states, attention_mask)["sample"]
+
+            # 2. compute previous image: x_t -> t_t-1
+            image = self.scheduler.step(model_output, t, image, generator=generator)["prev_sample"]
+
+        image = (image / 2 + 0.5).clamp(0, 1)
+        image = image.cpu().permute(0, 2, 3, 1).numpy()
+        if output_type == "pil":
+            image = self.numpy_to_pil(image)
+
+        return {"sample": image}
--- src/diffusers/schedulers/scheduling_ddpm.py
+++ src/diffusers/schedulers/scheduling_ddpm.py
@@ -179,6 +179,58 @@ class DDPMScheduler(SchedulerMixin, ConfigMixin):
 
         return {"prev_sample": pred_prev_sample}
 
+    def step_clean(
+        self,
+        model_output: Union[torch.FloatTensor, np.ndarray],
+        timestep: int,
+        sample: Union[torch.FloatTensor, np.ndarray],
+        predict_epsilon=True,
+        generator=None,
+    ):
+        t = timestep
+
+        if model_output.shape[1] == sample.shape[1] * 2 and self.variance_type in ["learned", "learned_range"]:
+            model_output, predicted_variance = torch.split(model_output, sample.shape[1], dim=1)
+        else:
+            predicted_variance = None
+
+        # 1. compute alphas, betas
+        alpha_prod_t = self.alphas_cumprod[t]
+        alpha_prod_t_prev = self.alphas_cumprod[t - 1] if t > 0 else self.one
+        beta_prod_t = 1 - alpha_prod_t
+        beta_prod_t_prev = 1 - alpha_prod_t_prev
+
+        # 2. compute predicted original sample from predicted noise also called
+        # "predicted x_0" of formula (15) from https://arxiv.org/pdf/2006.11239.pdf
+        if predict_epsilon:
+            pred_original_sample = (sample - beta_prod_t ** (0.5) * model_output) / alpha_prod_t ** (0.5)
+        else:
+            assert False
+            pred_original_sample = model_output
+
+        # 3. Clip "predicted x_0"
+        if self.config.clip_sample:
+            pred_original_sample = self.clip(pred_original_sample, -1, 1)
+
+        # 4. Compute coefficients for pred_original_sample x_0 and current sample x_t
+        # See formula (7) from https://arxiv.org/pdf/2006.11239.pdf
+        pred_original_sample_coeff = (alpha_prod_t_prev ** (0.5) * self.betas[t]) / beta_prod_t
+        current_sample_coeff = self.alphas[t] ** (0.5) * beta_prod_t_prev / beta_prod_t
+
+        # 5. Compute predicted previous sample µ_t
+        # See formula (7) from https://arxiv.org/pdf/2006.11239.pdf
+        pred_prev_sample = pred_original_sample_coeff * pred_original_sample + current_sample_coeff * sample
+
+        # 6. Add noise
+        variance = 0
+        if t > 0:
+            noise = self.randn_like(model_output, generator=generator)
+            variance = (self._get_variance(t, predicted_variance=predicted_variance) ** 0.5) * noise
+
+        pred_prev_sample = pred_prev_sample + variance
+
+        return {"prev_sample": pred_prev_sample, 'orig_sample': pred_original_sample}
+
     def add_noise(self, original_samples, noise, timesteps):
         sqrt_alpha_prod = self.alphas_cumprod[timesteps] ** 0.5
         sqrt_alpha_prod = self.match_shape(sqrt_alpha_prod, original_samples)
